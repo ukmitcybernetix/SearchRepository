ef907bc8e1591ad8df6f74a2349ff6c6e9c6717d
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-10-23 00:46:31 +0700

    Update ViewModel

--- app/src/main/java/com/zendev/searchrepository/ui/MainViewModel.kt
+++ app/src/main/java/com/zendev/searchrepository/ui/MainViewModel.kt
@@ -1,4 +1,41 @@
 package com.zendev.searchrepository.ui
 
-class MainViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import com.zendev.searchrepository.data.MainRepository
+import com.zendev.searchrepository.data.local.entity.Repository
+import com.zendev.searchrepository.data.local.entity.RepositoryResult
+
+class MainViewModel(private val repository: MainRepository) : ViewModel() {
+
+    companion object {
+        private const val VISIBLE_THRESHOLD = 5
+    }
+
+    private val queryLiveData = MutableLiveData<String>()
+    private val repoResult: LiveData<RepositoryResult> = Transformations.map(queryLiveData) {
+        repository.search(it)
+    }
+
+    val repos: LiveData<List<Repository>> = Transformations.switchMap(repoResult) { it -> it.data }
+    val networkErrors: LiveData<String> = Transformations.switchMap(repoResult) { it ->
+        it.networkErrors
+    }
+
+    fun searchRepo(queryString: String) {
+        queryLiveData.postValue(queryString)
+    }
+
+    fun listScrolled(visibleItemCount: Int, lastVisibleItemPosition: Int, totalItemCount: Int) {
+        if (visibleItemCount + lastVisibleItemPosition + VISIBLE_THRESHOLD >= totalItemCount) {
+            val immutableQuery = lastQueryValue()
+            if (immutableQuery != null) {
+                repository.requestMore(immutableQuery)
+            }
+        }
+    }
+
+    fun lastQueryValue(): String? = queryLiveData.value
 }
--- app/src/main/java/com/zendev/searchrepository/viewmodel/ViewModelFactory.kt
+++ app/src/main/java/com/zendev/searchrepository/viewmodel/ViewModelFactory.kt
@@ -1,4 +1,17 @@
 package com.zendev.searchrepository.viewmodel
 
-class ViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.zendev.searchrepository.data.MainRepository
+import com.zendev.searchrepository.ui.MainViewModel
+
+class ViewModelFactory(private val repository: MainRepository) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return MainViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
