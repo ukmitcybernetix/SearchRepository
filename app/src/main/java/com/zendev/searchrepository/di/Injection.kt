7b9c942fe5d88c8b900e715bd0c8a3a79e5b9e81
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-10-23 00:42:58 +0700

    Update Injection

--- app/src/main/java/com/zendev/searchrepository/di/Injection.kt
+++ app/src/main/java/com/zendev/searchrepository/di/Injection.kt
@@ -1,4 +1,26 @@
 package com.zendev.searchrepository.di
 
+import android.content.Context
+import androidx.lifecycle.ViewModelProvider
+import com.zendev.searchrepository.data.MainRepository
+import com.zendev.searchrepository.data.local.GithubLocalCache
+import com.zendev.searchrepository.data.local.RepositoryDatabase
+import com.zendev.searchrepository.data.remote.GithubService
+import com.zendev.searchrepository.viewmodel.ViewModelFactory
+import java.util.concurrent.Executors
+
 object Injection {
+
+    private fun provideCache(context: Context): GithubLocalCache {
+        val database = RepositoryDatabase.getInstance(context)
+        return GithubLocalCache(database.repositoryDao(), Executors.newSingleThreadExecutor())
+    }
+
+    private fun provideGithubRepository(context: Context): MainRepository {
+        return MainRepository(GithubService.create(), provideCache(context))
+    }
+
+    fun provideViewModelFactory(context: Context): ViewModelProvider.Factory {
+        return ViewModelFactory(provideGithubRepository(context))
+    }
 }
