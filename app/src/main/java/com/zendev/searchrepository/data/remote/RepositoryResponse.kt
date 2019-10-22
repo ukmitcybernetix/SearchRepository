cc7f7f0206280f7714f92cb370b70180f5c96ef6
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-10-23 00:22:10 +0700

    Update Repository Response

--- app/src/main/java/com/zendev/searchrepository/data/remote/RepositoryResponse.kt
+++ app/src/main/java/com/zendev/searchrepository/data/remote/RepositoryResponse.kt
@@ -1,4 +1,10 @@
 package com.zendev.searchrepository.data.remote
 
-class RepositoryResponse {
-}
+import com.google.gson.annotations.SerializedName
+import com.zendev.searchrepository.data.local.entity.Repository
+
+data class RepositoryResponse(
+    @SerializedName("total_count") val total: Int = 0,
+    @SerializedName("items") val items: List<Repository> = emptyList(),
+    val nextPage: Int? = null
+)
