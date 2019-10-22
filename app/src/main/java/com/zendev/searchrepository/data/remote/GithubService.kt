bcbeb9d8f0be8ddb5c7ec59a5b3daaebecdec40a
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-10-23 00:20:58 +0700

    Add Fetch Data From Network

--- app/src/main/java/com/zendev/searchrepository/data/remote/GithubService.kt
+++ app/src/main/java/com/zendev/searchrepository/data/remote/GithubService.kt
@@ -1,4 +1,79 @@
 package com.zendev.searchrepository.data.remote
 
-object GithubService {
+import android.util.Log
+import com.zendev.searchrepository.data.local.entity.Repository
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+private const val TAG = "GithubService"
+private const val IN_QUALIFIER = "in:name,description"
+
+fun searchRepos(
+    service: GithubService,
+    query: String,
+    page: Int,
+    itemsPerPage: Int,
+    onSuccess: (repos: List<Repository>) -> Unit,
+    onError: (error: String) -> Unit
+) {
+    Log.d(TAG, "query: $query, page: $page, itemsPerPage: $itemsPerPage")
+
+    val apiQuery = query + IN_QUALIFIER
+
+    service.searchRepos(apiQuery, page, itemsPerPage).enqueue(
+        object : Callback<RepositoryResponse> {
+            override fun onFailure(call: Call<RepositoryResponse>?, t: Throwable) {
+                Log.d(TAG, "fail to get data")
+                onError(t.message ?: "unknown error")
+            }
+
+            override fun onResponse(
+                call: Call<RepositoryResponse>?,
+                response: Response<RepositoryResponse>
+            ) {
+                Log.d(TAG, "got a response $response")
+                if (response.isSuccessful) {
+                    val repos = response.body()?.items ?: emptyList()
+                    onSuccess(repos)
+                } else {
+                    onError(response.errorBody()?.string() ?: "Unknown error")
+                }
+            }
+        }
+    )
+}
+
+interface GithubService {
+    @GET("search/repositories?sort=stars")
+    fun searchRepos(
+        @Query("q") query: String,
+        @Query("page") page: Int,
+        @Query("per_page") itemsPerPage: Int
+    ): Call<RepositoryResponse>
+
+    companion object {
+        private const val BASE_URL = "https://api.github.com/"
+
+        fun create(): GithubService {
+            val logger = HttpLoggingInterceptor()
+            logger.level = HttpLoggingInterceptor.Level.BASIC
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logger)
+                .build()
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(GithubService::class.java)
+        }
+    }
 }
