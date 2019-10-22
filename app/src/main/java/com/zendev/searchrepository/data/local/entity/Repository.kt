18fc021c21e75c5c321efff735a9e43fec3db3d4
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-10-23 00:14:31 +0700

    Update Repository

--- app/src/main/java/com/zendev/searchrepository/data/local/entity/Repository.kt
+++ app/src/main/java/com/zendev/searchrepository/data/local/entity/Repository.kt
@@ -4,16 +4,15 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
-class Repository {
-    @Entity(tableName = "repos")
-    data class Repo(
-        @PrimaryKey @field:SerializedName("id") val id: Long,
-        @field:SerializedName("name") val name: String,
-        @field:SerializedName("full_name") val fullName: String,
-        @field:SerializedName("description") val description: String?,
-        @field:SerializedName("html_url") val url: String,
-        @field:SerializedName("stargazers_count") val stars: Int,
-        @field:SerializedName("forks_count") val forks: Int,
-        @field:SerializedName("language") val language: String?
-    )
-}
+@Entity(tableName = "repos")
+class Repository(
+    @PrimaryKey
+    @field:SerializedName("id") val id: Long,
+    @field:SerializedName("name") val name: String,
+    @field:SerializedName("full_name") val fullName: String,
+    @field:SerializedName("description") val description: String?,
+    @field:SerializedName("html_url") val url: String,
+    @field:SerializedName("stargazers_count") val stars: Int,
+    @field:SerializedName("forks_count") val forks: Int,
+    @field:SerializedName("language") val language: String?
+)
