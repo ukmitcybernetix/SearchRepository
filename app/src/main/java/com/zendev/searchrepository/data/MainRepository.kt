2847003a88074a0c22a0a54448e2aa9c241b4c77
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-10-23 00:40:30 +0700

    Update MainRepository

--- app/src/main/java/com/zendev/searchrepository/data/MainRepository.kt
+++ app/src/main/java/com/zendev/searchrepository/data/MainRepository.kt
@@ -1,4 +1,50 @@
 package com.zendev.searchrepository.data
 
-class MainRepository {
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import com.zendev.searchrepository.data.local.GithubLocalCache
+import com.zendev.searchrepository.data.local.entity.RepositoryResult
+import com.zendev.searchrepository.data.remote.GithubService
+import com.zendev.searchrepository.data.remote.searchRepos
+
+class MainRepository(
+    private val service: GithubService,
+    private val cache: GithubLocalCache
+) {
+    private var lastRequestedPage = 1
+    private val networkErrors = MutableLiveData<String>()
+    private var isRequestInProgress = false
+
+    fun search(query: String): RepositoryResult {
+        Log.d("GithubRepository", "New query: $query")
+        lastRequestedPage = 1
+        requestAndSaveData(query)
+
+        val data = cache.reposByName(query)
+
+        return RepositoryResult(data, networkErrors)
+    }
+
+    fun requestMore(query: String) {
+        requestAndSaveData(query)
+    }
+
+    private fun requestAndSaveData(query: String) {
+        if (isRequestInProgress) return
+
+        isRequestInProgress = true
+        searchRepos(service, query, lastRequestedPage, NETWORK_PAGE_SIZE, { repos ->
+            cache.insert(repos) {
+                lastRequestedPage++
+                isRequestInProgress = false
+            }
+        }, { error ->
+            networkErrors.postValue(error)
+            isRequestInProgress = false
+        })
+    }
+
+    companion object {
+        private const val NETWORK_PAGE_SIZE = 50
+    }
 }
--- app/src/main/java/com/zendev/searchrepository/data/local/GithubLocalCache.kt
+++ app/src/main/java/com/zendev/searchrepository/data/local/GithubLocalCache.kt
@@ -1,4 +1,25 @@
 package com.zendev.searchrepository.data.local
 
-class GithubLocalCache {
+import android.util.Log
+import androidx.lifecycle.LiveData
+import com.zendev.searchrepository.data.local.entity.Repository
+import java.util.concurrent.Executor
+
+class GithubLocalCache(
+    private val repoDao: RepositoryDao,
+    private val ioExecutor: Executor
+) {
+    fun insert(repos: List<Repository>, insertFinished: () -> Unit) {
+        ioExecutor.execute {
+            Log.d("GithubLocalCache", "inserting ${repos.size} repos")
+            repoDao.insert(repos)
+            insertFinished()
+        }
+    }
+
+    fun reposByName(name: String): LiveData<List<Repository>> {
+        // appending '%' so we can allow other characters to be before and after the query string
+        val query = "%${name.replace(' ', '%')}%"
+        return repoDao.reposByName(query)
+    }
 }
