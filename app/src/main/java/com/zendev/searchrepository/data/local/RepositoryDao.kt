4a96035b215ad434df252bc29f5abf0e3f7ec77a
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-10-23 00:27:35 +0700

    Update Repository Database and Repository DAO

--- app/src/main/java/com/zendev/searchrepository/data/local/RepositoryDao.kt
+++ app/src/main/java/com/zendev/searchrepository/data/local/RepositoryDao.kt
@@ -1,4 +1,19 @@
 package com.zendev.searchrepository.data.local
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.zendev.searchrepository.data.local.entity.Repository
+
+@Dao
 interface RepositoryDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(posts: List<Repository>)
+
+    @Query("SELECT * FROM repos WHERE (name LIKE :queryString) OR (description LIKE " +
+            ":queryString) ORDER BY stars DESC, name ASC")
+    fun reposByName(queryString: String): LiveData<List<Repository>>
 }
--- app/src/main/java/com/zendev/searchrepository/data/local/RepositoryDatabase.kt
+++ app/src/main/java/com/zendev/searchrepository/data/local/RepositoryDatabase.kt
@@ -1,4 +1,34 @@
 package com.zendev.searchrepository.data.local
 
-abstract class RepositoryDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.zendev.searchrepository.data.local.entity.Repository
+
+@Database(
+    entities = [Repository::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class RepositoryDatabase : RoomDatabase() {
+
+    abstract fun repositoryDao(): RepositoryDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: RepositoryDatabase? = null
+
+        fun getInstance(context: Context): RepositoryDatabase =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE
+                    ?: buildDatabase(context).also { INSTANCE = it }
+            }
+
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(context.applicationContext,
+                RepositoryDatabase::class.java, "repository.db")
+                .build()
+    }
 }
